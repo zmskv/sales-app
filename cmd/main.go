5c9284ebbb1bc1eefb7b907090435be84ce21dea
zmskv <zmskvxd@gmail.com>
2024-07-04 01:09:46 +0300

    config and docker added

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,31 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	"github.com/zmskv/sales-app"
 	"github.com/zmskv/sales-app/internal/handler"
+	"github.com/zmskv/sales-app/internal/repository"
+	"github.com/zmskv/sales-app/internal/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error Initializing config: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	server := new(sales.Server)
-	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("internal/config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
