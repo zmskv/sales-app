8e15bb714dc2ea7da6b09168ebb9ab815aac2dc2
zmskv <zmskvxd@gmail.com>
2024-07-04 13:34:34 +0300

    update models

--- internal/model/product.go
+++ internal/model/product.go
@@ -8,6 +8,7 @@ type Product struct {
 	Id     int       `json:"id"`
 	Title  string    `json:"title"`
 	Amount int       `json:"amount"`
-	Total  int       `json:"summa"`
+	Price  int       `json:"price"`
+	Total  int       `json:"total"`
 	Date   time.Time `json:"date"`
 }
