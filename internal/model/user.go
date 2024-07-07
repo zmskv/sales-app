7b4804aa4ffe92be978b306e22bfc907d23da42a
zmskv <zmskvxd@gmail.com>
2024-07-07 16:07:29 +0300

    update user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type User struct {
-	Id        string    `json:"id"`
-	Username  string    `json:"username" binding:"required"`
-	Password  string    `json:"password" binding:"required"`
-	CreatedAt time.Time `json:"created_at"`
+	Id        string
+	Username  string `json:"username" binding:"required"`
+	Password  string `json:"password" binding:"required"`
+	CreatedAt time.Time
 }
