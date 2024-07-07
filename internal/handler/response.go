2940c48b72595bf5e6b835c1855dc44235cdf9a7
zmskv <zmskvxd@gmail.com>
2024-07-07 16:06:32 +0300

    added SuccessResponse

--- internal/handler/response.go
+++ internal/handler/response.go
@@ -6,6 +6,14 @@ type ErrorResponse struct {
 	Message string `json:"message"`
 }
 
+type SuccessResponse struct {
+	Message string `json:"message"`
+}
+
 func NewErrorResponse(c *gin.Context, statusCode int, message string) {
 	c.AbortWithStatusJSON(statusCode, ErrorResponse{message})
+}
+
+func NewSuccessResponse(c *gin.Context, statusCode int, message string) {
+	c.AbortWithStatusJSON(statusCode, SuccessResponse{message})
 }
